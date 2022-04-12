cb3865bd155d31e2b65b193079a6dc374b04c006
darsen <darsenungarlinov00@gmail.com>
2022-04-12 11:32:08 +0300

    added styles

--- src/app/app.controller.ts
+++ src/app/app.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Get, Res } from '@nestjs/common';
 import { AppService } from './app.service';
+import path from 'path';
 
 @Controller()
 export class AppController {
@@ -7,8 +8,6 @@ export class AppController {
 
   @Get()
   index(@Res() res) {
-    res.send(
-      'Available routes: /albums, /comments, /photos, /posts, /todos, /users',
-    );
+    res.sendFile(`${process.env.PWD}/src/index.html`);
   }
 }
