dae7f38dce271b146d63993bdb7d84656726ae80
darsen <darsenungarlinov00@gmail.com>
2022-06-16 15:09:54 +0300

    py spaces

--- test/pythonTest.py
+++ test/pythonTest.py
@@ -1,248 +1,133 @@
 import requests
 
 BASE_URL = "https://jsonplaceholder-copy.vercel.app/todos/"
-
 BASE_URL2 = "https://jsonplaceholder-copy.vercel.app/users/"
-
 BASE_URL3 = "https://jsonplaceholder-copy.vercel.app/posts/"
-
 BASE_URL4 = "https://jsonplaceholder-copy.vercel.app/comments/"
-
 BASE_URL5 = "https://jsonplaceholder-copy.vercel.app/albums/"
-
 BASE_URL6 = "https://jsonplaceholder-copy.vercel.app/photos/"
 
 def find_todos() -> dict:
     resp = requests.get(BASE_URL)
     return resp.json()
-
 def find_users() -> dict:
     resp = requests.get(BASE_URL2)
     return resp.json()
-
 def find_posts() -> dict:
     resp = requests.get(BASE_URL3)
     return resp.json()
-
 def find_comments() -> dict:
     resp = requests.get(BASE_URL4)
     return resp.json()
-
 def find_albums() -> dict:
     resp = requests.get(BASE_URL5)
     return resp.json()
-
 def find_photos() -> dict:
     resp = requests.get(BASE_URL6)
     return resp.json()
 
 print(find_albums())
-
 print(find_comments())
-
 print(find_photos())
-
 print(find_posts())
-
 print(find_todos())
-
 print(find_users())
-
 print(find_albums())
-
 print(find_albums())
-
 print(find_comments())
-
 print(find_photos())
-
 print(find_posts())
-
 print(find_todos())
-
 print(find_users())
-
 print(find_albums())
-
 print(find_albums())
-
 print(find_comments())
-
 print(find_photos())
-
 print(find_posts())
-
 print(find_todos())
-
 print(find_users())
-
 print(find_albums())
-
 print(find_albums())
-
 print(find_comments())
-
 print(find_photos())
-
 print(find_posts())
-
 print(find_todos())
-
 print(find_users())
-
 print(find_albums())
-
 print(find_albums())
-
 print(find_comments())
-
 print(find_photos())
-
 print(find_posts())
-
 print(find_todos())
-
 print(find_users())
-
 print(find_albums())
-
 print(find_albums())
-
 print(find_comments())
-
 print(find_photos())
-
 print(find_posts())
-
 print(find_todos())
-
 print(find_users())
-
 print(find_albums())
-
 print(find_albums())
-
 print(find_comments())
-
 print(find_photos())
-
 print(find_posts())
-
 print(find_todos())
-
 print(find_users())
-
 print(find_albums())
-
 print(find_albums())
-
 print(find_comments())
-
 print(find_photos())
-
 print(find_posts())
-
 print(find_todos())
-
 print(find_users())
-
 print(find_albums())
-
 print(find_albums())
-
 print(find_comments())
-
 print(find_photos())
-
 print(find_posts())
-
 print(find_todos())
-
 print(find_users())
-
 print(find_albums())
-
 print(find_albums())
-
 print(find_comments())
-
 print(find_photos())
-
 print(find_posts())
-
 print(find_todos())
-
 print(find_users())
-
 print(find_albums())
-
 print(find_albums())
-
 print(find_comments())
-
 print(find_photos())
-
 print(find_posts())
-
 print(find_todos())
-
 print(find_users())
-
 print(find_albums())
-
 print(find_albums())
-
 print(find_comments())
-
 print(find_photos())
-
 print(find_posts())
-
-
 print(find_todos())
-
 print(find_users())
-
 print(find_albums())
-
 print(find_albums())
-
 print(find_comments())
-
 print(find_photos())
-
 print(find_posts())
-
 print(find_todos())
-
 print(find_users())
-
 print(find_albums())
-
 print(find_albums())
-
 print(find_comments())
-
 print(find_photos())
-
 print(find_posts())
-
 print(find_todos())
-
 print(find_users())
-
 print(find_albums())
-
 print(find_albums())
-
 print(find_comments())
-
 print(find_photos())
-
 print(find_posts())
-
 print(find_todos())
-
 print(find_users())
-
 print(find_albums())
